e8fbaceb3eabd7dd6d1ae70e0f5e726c7c198871
ploteen <ploteen@g.skku.edu>
2021-08-11 16:53:19 +0900

    [ADD] Fragement를 이용한 nav바 구현
    이후 setting fragment와 서버 통신 제작 예정
    그이후 필요한 image 제작 이후 나머지 fragment 제작

--- App/app/src/main/java/com/example/cyberbullingdetect/MainActivity.java
+++ App/app/src/main/java/com/example/cyberbullingdetect/MainActivity.java
@@ -1,20 +1,25 @@
 package com.example.cyberbullingdetect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText text_id;
-    Button btn_id;
-
+    private BottomNavigationView mBottomNV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +27,51 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
         startActivity(intent);
-        text_id = findViewById(R.id.text_id);
-        btn_id = findViewById(R.id.btn_id);
+
+        mBottomNV = findViewById(R.id.nav_view);
+        mBottomNV.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                BottomNavigate(menuItem.getItemId());
+
+
+                return true;
+            } //NavigationItemSelecte
+        });
+        mBottomNV.setSelectedItemId(R.id.navigation_1);
+    }
+
+    private void BottomNavigate(int id) {  //BottomNavigation 페이지 변경
+        String tag = String.valueOf(id);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        Fragment currentFragment = fragmentManager.getPrimaryNavigationFragment();
+        if (currentFragment != null) {
+            fragmentTransaction.hide(currentFragment);
+        }
+
+        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+        if (fragment == null) {
+            if (id == R.id.navigation_1) {
+                fragment = new ScoreActivity();
+
+            } else if (id == R.id.navigation_2){
+
+                fragment = new HelpActivity();
+            }else {
+                fragment = new SettingsActivity();
+            }
+
+            fragmentTransaction.add(R.id.content_layout, fragment, tag);
+        } else {
+            fragmentTransaction.show(fragment);
+        }
+
+        fragmentTransaction.setPrimaryNavigationFragment(fragment);
+        fragmentTransaction.setReorderingAllowed(true);
+        fragmentTransaction.commitNow();
+
 
     }
 }
---
+++ App/app/src/main/java/com/example/cyberbullingdetect/SettingsActivity.java
@@ -0,0 +1,26 @@
+package com.example.cyberbullingdetect;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingsActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.settings_activity, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
