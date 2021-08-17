f621bbbb655d07645c6a00f7caf2fe968d82c93a
ploteen <ploteen@g.skku.edu>
2021-08-17 17:04:16 +0900

    [ADD] 도우미 창 이미지 슬라이드 생성
    viewpager2 와 glide를 이용해서 도우미 이미지 생성
    
    main frame 점수창 추후 생성예정

--- App/app/src/main/java/com/example/cyberbullingdetect/HelpActivity.java
+++ App/app/src/main/java/com/example/cyberbullingdetect/HelpActivity.java
@@ -3,24 +3,90 @@ package com.example.cyberbullingdetect;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class HelpActivity extends Fragment {
 
+    private ViewGroup viewGroup;
+    private ViewPager2 sliderViewPager;
+    private LinearLayout layoutIndicator;
+
+    private String[] images = new String[] {
+            "ic_launcher_background",
+            "ic_launcher_foreground",
+            "ic_launcher_background",
+            "ic_launcher_foreground",
+            "ic_launcher_background"
+    };
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
-        return inflater.inflate(R.layout.activity_help, container, false);
+        viewGroup = (ViewGroup) inflater.inflate(R.layout.activity_help, container, false);
+
+        sliderViewPager = viewGroup.findViewById(R.id.sliderViewPager);
+        layoutIndicator = viewGroup.findViewById(R.id.layoutIndicators);
+
+        sliderViewPager.setOffscreenPageLimit(1);
+        sliderViewPager.setAdapter(new ImageSliderAdapter(getContext(), images));
+
+        sliderViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentIndicator(position);
+            }
+        });
+
+        setupIndicators(images.length);
+        return viewGroup;
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+    private void setupIndicators(int count) {
+        ImageView[] indicators = new ImageView[count];
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        params.setMargins(16, 8, 16, 8);
+
+        for (int i = 0; i < indicators.length; i++) {
+            indicators[i] = new ImageView(getContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(getContext(),
+                    R.drawable.bg_indicator_inactive));
+            indicators[i].setLayoutParams(params);
+            layoutIndicator.addView(indicators[i]);
+        }
+        setCurrentIndicator(0);
+    }
+
+    private void setCurrentIndicator(int position) {
+        int childCount = layoutIndicator.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            ImageView imageView = (ImageView) layoutIndicator.getChildAt(i);
+            if (i == position) {
+                imageView.setImageDrawable(ContextCompat.getDrawable(
+                        getContext(),
+                        R.drawable.bg_indicator_active
+                ));
+            } else {
+                imageView.setImageDrawable(ContextCompat.getDrawable(
+                        getContext(),
+                        R.drawable.bg_indicator_inactive
+                ));
+            }
+        }
     }
 
 }
