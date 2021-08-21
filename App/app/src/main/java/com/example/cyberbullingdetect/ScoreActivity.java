0fa97742a5809085eae8849be83c730064d7d7da
ploteen <ploteen@g.skku.edu>
2021-08-21 13:35:05 +0900

    [ADD] 설정 화면 & 전화 다이얼 기능 추가
    설정 화면에서 정한 도움 받는 사람에 대한 다이얼 기능 추가
    
    추후 서버 구성 및 notification 통신을 통해 스코어를 저장하는 기능을 추가할 예정

--- App/app/src/main/java/com/example/cyberbullingdetect/ScoreActivity.java
+++ App/app/src/main/java/com/example/cyberbullingdetect/ScoreActivity.java
@@ -4,23 +4,77 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ScoreActivity extends Fragment {
 
+    private View view;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
+    private TextView name;
+    private Button helper_btn;
+    private Button cyber_btn;
+    private Button police_btn;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
-        return inflater.inflate(R.layout.activity_score, container, false);
+        view = inflater.inflate(R.layout.activity_score, container, false);
+        preferences = PreferenceManager.getDefaultSharedPreferences(view.getContext());
+        editor = preferences.edit();
+        name = view.findViewById(R.id.helper_name);
+        //name 바꾸기
+        name.setText(preferences.getString("name","없음"));
+        //helper button 바꾸기
+        helper_btn = view.findViewById(R.id.helper_button);
+        String num = preferences.getString("number","0");
+
+        helper_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + num));
+                startActivity(intent);
+            }
+        });
+        //cyber button
+        cyber_btn = view.findViewById(R.id.cyber_button);
+        cyber_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:1388"));
+                startActivity(intent);
+            }
+        });
+        //police button
+        police_btn = view.findViewById(R.id.police_button);
+        police_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:117"));
+                startActivity(intent);
+            }
+        });
+        return view;
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
-
 }
--- App/app/src/main/java/com/example/cyberbullingdetect/SettingsActivity.java
+++ App/app/src/main/java/com/example/cyberbullingdetect/SettingsActivity.java
@@ -4,23 +4,50 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SettingsActivity extends Fragment {
+public class SettingsActivity extends Fragment implements View.OnClickListener{
 
+    private Button btn;
+    private EditText name;
+    private EditText number;
+    private View root ;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
-        return inflater.inflate(R.layout.settings_activity, container, false);
+        root = inflater.inflate(R.layout.settings_activity, container, false);
+        btn = root.findViewById(R.id.btn);
+        name = root.findViewById(R.id.name);
+        number = root.findViewById(R.id.number);
+        btn.setOnClickListener(this);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(root.getContext());
+        editor = preferences.edit();
+        return root;
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+    @Override
+    public void onClick(View view){
+        String n = name.getText().toString();
+        String num = number.getText().toString();
+        editor.putString("name", n);
+        editor.putString("number",num);
+        editor.apply();
+
     }
 
 }
